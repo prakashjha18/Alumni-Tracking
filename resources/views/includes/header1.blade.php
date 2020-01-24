49a548ae15a88fa7ef7de8ed568aa981d56a366f
Sanya <gandhisanya99@gmail.com>
2020-01-24 11:29:40 +0530

    College page done

--- resources/views/includes/header1.blade.php
+++ resources/views/includes/header1.blade.php
@@ -8,12 +8,12 @@
                 <div class="col-lg-12 main-menu navbar navbar-expand-lg navbar-light">
                     <div class=" col-lg-9 col-md-9">
                         <a class="navbar-brand" href="index.html">
-                            <img src="assets\img\logo.png" alt="Logo"style="height: 60%; width: 60%;">
+                            <img src="assets\img\logo.png" alt="Logo" style="height: 14vh; width: 10vw;">
                         </a>
                     </div>
                     <div class="col-lg-3 col-md-3 ">
                         <a class="navbar-brand" href="index.html">
-                            <img src="assets\img\logo2.jfif" alt="Logo" style="height: 60%; width: 60%;">
+                            <img src="assets\img\logo2.jfif" alt="Logo"style="height: 14vh; width: 12vw;" >
                         </a>
                     </div>
 
