de2a648e331dd7361ed6e2f6f982eb0d9f22c7f2
Rajesh <rajeshjha99.rj@gmail.com>
2021-01-06 20:31:57 +0530

    fixed update

--- resources/views/college/editevent.blade.php
+++ resources/views/college/editevent.blade.php
@@ -29,18 +29,7 @@
             <label for="description">Description</label>
             <textarea class="form-control" id="description" name="description" rows="5" required>{{$event->description}}</textarea>
         </div>
-        <div class="form-group">
-            <label for="fileinput">Image</label>
-            <input type="file" class="form-control-file" name="imagef" value="{{asset($event->image)}}" required>
-        </div>
-        <div class="form-group">
-            <label for="modes">Mode</label>
-            <select class="form-control" name="mode" value="{{$event->mode}}" id="modes">
-                <option>Online</option>
-                <option>Offline</option>
-            </select>
-        </div>
-
+        
         <button type="submit" class="btn btn-primary">Submit</button>
         </form>
      </div>
