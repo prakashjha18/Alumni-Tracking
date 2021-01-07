683d84c02b0f3e2cc8e6428c6278e13730d370e3
Rajesh <rajeshjha99.rj@gmail.com>
2021-01-07 17:23:09 +0530

    location fixed

--- resources/views/directorate/location.blade.php
+++ resources/views/directorate/location.blade.php
@@ -13,27 +13,29 @@
       </ol>
       <h3>COLLEGE LOCATION</h3>
     <div class="container ">
-      @foreach($colleges as $college)
         <div class="panel-group" id="faqAccordion" style="font-family:cursive">
             <div class="panel panel-default ">
                 <div class="panel-heading accordion-toggle question-toggle collapsed" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question0">
-                     <h5 class="panel-title"><u>{{$college->orgname}}</u></h5>
-    
+                     <h5 class="panel-title"><u>Colleges</u></h5>  
                 </div>
                 <div id="question0" class="panel-collapse collapse" style="height: 0px;">
                     <div class="panel-body">
                          <div id="map-container-google-3" class="z-depth-1-half map-container-3">
+                          @foreach($colleges as $college)
                             <iframe src="https://maps.google.com/maps?q={{$college->orgname}}&t=k&z=13&ie=UTF8&iwloc=&output=embed" id="map" style="width:100%;height:50% "frameborder="0"
-                              style="border:0" allowfullscreen></iframe>
+                              style="border:0" allowfullscreen></iframe> 
+                              <br><br><br>
+                          @endforeach
                           </div>
-                    </div>
+                    </div> 
                 </div>
               </div>
         </div>
-      @endforeach
     </div>
     </div>
 </div>
+<br>
+
 <footer class="sticky-footer">
     <div class="container">
       <div class="text-center">
